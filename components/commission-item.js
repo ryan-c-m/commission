5ba1b5814ad0076108fea2bd9ec360bf920166f2
Ryan Morris <ryan.c.morris1@gmail.com>
2018-05-28 23:11:45 +1000

    adds includes GST

--- components/commission-item.js
+++ components/commission-item.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import NumericInput from 'react-native-numeric-input'
 
 export default class CommissionItem extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(val) {
+        this.props.value(val);
+        this.state.value = val;
     }
 
     render() {
@@ -13,14 +20,23 @@ export default class CommissionItem extends React.Component {
             <View style={itemstyles.icontainer}>
                 <Text style={itemstyles.text}>{this.props.title}</Text>
                 <View style={itemstyles.input}>
-                    {this.props.type !== 'percent' ? <Text style={itemstyles.text}>$</Text> : null}
+                    {this.props.type !== 'percent' ? <Text style={this.state.value ? itemstyles.dollar : itemstyles.dollarNoValue}>$</Text> : null}
                     <TextInput
-                        style={{ fontSize: 30 }}
+                        style={{
+                            fontSize: 20,
+                            color: '#000000',
+                            margin: 5
+                        }}
                         placeholder="0"
-                        onChangeText={val => this.props.value(val)}
-                        keyboardType="numeric"
+                        placeholderTextColor="#bbbbbb"
+                        onChangeText={val => this.handleChange(val)}
+                        keyboardType="phone-pad"
+                        multiline={false}
+                        underlineColorAndroid="transparent"
+                        selectTextOnFocus
                     />
-                    {this.props.type === 'percent' ? <Text style={itemstyles.text}>%</Text> : null}
+                    {this.props.type === 'percent' ? <Text style={this.state.value ?
+                        itemstyles.dollar : itemstyles.dollarNoValue}>%</Text> : null}
                 </View>
             </View>
         );
@@ -31,16 +47,35 @@ const itemstyles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        backgroundColor: 'transparent',
+        maxHeight: 100
     },
     text: {
-        fontSize: 25,
-        fontFamily: 'sans-serif'
+        fontSize: 20,
+        fontFamily: 'sans-serif',
+        color: '#000000'
+    },
+    dollar: {
+        fontSize: 20,
+        fontFamily: 'sans-serif',
+        color: '#000000'
+    },
+    dollarNoValue: {
+        fontSize: 20,
+        fontFamily: 'sans-serif',
+        color: '#bbbbbb'
     },
     input: {
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'flex-end'
+        justifyContent: 'flex-end',
+        borderRadius: 5,
+        borderBottomWidth: 1.5,
+        borderColor: 'rgb(211,211,211)',
+        backgroundColor: 'white',
+        maxWidth: 120,
+        padding: 5
     }
 });
--- components/commission.js
+++ components/commission.js
@@ -1,58 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Switch } from 'react-native';
+
 import CommissionItem from './commission-item'
 
 export default class Commission extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { salePrice: 0, agentFee: 0, total: 0 };
+        this.state = { salePrice: 0, agentFee: 0, gst: false };
+        this.rawCommission = this.rawCommission.bind(this);
         this.commission = this.commission.bind(this);
+        this.gst = this.gst.bind(this);
+
+    }
+
+    rawCommission() {
+        return commissionValue = parseFloat(this.state.salePrice) * (parseFloat(this.state.agentFee) / 100);
     }
 
     commission() {
-        return parseInt(this.state.salePrice) * (parseInt(this.state.agentFee) / 100);
+        const commissionValue = this.rawCommission();
+        return this.state.gst ? commissionValue - (commissionValue / 10) : commissionValue;
+    }
+
+    gst() {
+        const commissionValue = this.rawCommission();
+        return commissionValue / 10;
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>Commissions</Text>
+                <View style={styles.top}>
+                    <Text style={styles.total}>{this.commission() ? ("$" + this.commission().toFixed(2)) : ""}</Text>
+                    <Text style={styles.gst}>{this.commission() ? ("GST: $" + this.gst().toFixed(2)) : ""}</Text>
+                </View>
                 <View style={styles.itemcontainer}>
                     <CommissionItem value={(salePrice) => this.setState({ salePrice })} type="dollar" title="Sale price" />
                     <CommissionItem value={(agentFee) => this.setState({ agentFee })} type="percent" title="Agent fee" />
-                    <CommissionItem value={(advertising) => this.setState({ advertising })} type="dollar" title="Advertising" />
+                    <View style={{
+                        marginTop: 50, flex: 0, flexDirection: "row", alignItems: "center", justifyContent: "space-evenly"
+                    }}>
+                        <Text style={{ fontSize: 15 }}>Agent fee includes GST</Text>
+                        <Switch value={this.state.gst} onValueChange={(gst) => this.setState({ gst })} />
+                    </View>
                 </View>
-                <Text style={styles.total}>Commission: ${this.commission()}</Text>
-
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+
+    top: {
+        marginBottom: 20,
+        marginTop: 60,
+    },
     container: {
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: 'white'
     },
     itemcontainer: {
         flex: 1,
         flexDirection: 'column',
+        justifyContent: 'flex-start',
         backgroundColor: '#fff',
         alignItems: 'stretch',
-        minWidth: 300
-
-    },
-    title: {
-        fontSize: 30,
-        marginBottom: 20,
-        marginTop: 60
+        minWidth: 300,
+        backgroundColor: 'transparent',
+        backgroundColor: 'white',
+        padding: 20
     },
     total: {
+        fontSize: 35,
+        color: '#000000'
+    },
+    gst: {
+        marginTop: 20,
         fontSize: 20,
-        marginBottom: 20,
-        marginTop: 60
+
     }
 });
